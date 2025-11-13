095952eef2c4b69a915b6be80a03396538766673
DayXL <day.luc.xav0606@gmail.com>
2025-11-13 00:01:57 -0300

    feat: add constants

---
+++ my-app/src/constants/constants.js
@@ -0,0 +1,13 @@
+const HOME_PAGE = {
+    SEARCH_USER_IN: "Buscar um usuário no ",
+    GITHUB: "GitHub",
+    VERY: "ficou muito ",
+    EASY: "fácil!",
+    SEARCH_USER: "Buscar usuário",
+    LINK_BITWISE: "https://www.bitwisebrasil.com.br/",
+    SPAW_TEXT_1: "Nenhum usuário encontrado!",
+    SPAW_TEXT_2: "Enquanto isso, acompanhe a Bitwise nas redes sociais:",
+
+}
+
+export default HOME_PAGE;
---
+++ my-app/src/constants/footBar.js
@@ -0,0 +1,22 @@
+const FOOTER_BAR = {
+    VISION: 'Acreditamos no valor da tecnologia e no impacto \n' +
+            ' positivo que a transformação digital exerce sobre \n' +
+            'a sociedade, mudando o nosso dia a dia, facilitando \n' +
+            'atividades comuns e acelerando os negócios.',
+
+    ADDRESS_ONE: 'R. Raphael Dias da Silva, 75 \n' +
+                 '3º andar • Sala 306 e 308\n' +
+                 'Campolim • Sorocaba/SP',
+
+    ADDRESS_TWO: '830, Av. Ernest-Gagnon, \n' +
+                 'Bureau 315, Sala 609 \n' +
+                 'Quebec \n',
+
+    RIGHT_RESERVED: 'Todos os direitos reservados - 2020',
+
+    BRASIL: 'BRASIL',
+
+    CANADA: 'CANADÁ'
+};
+
+export default FOOTER_BAR;
